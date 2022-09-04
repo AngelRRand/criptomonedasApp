32fd4e600f625185353b15e440295722146c9813
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-04 18:34:27 -0300

    pciker

--- src/Form.js
+++ src/Form.js
@@ -3,25 +3,28 @@ import React, { useState } from 'react'
 import styles from '../styles/Form'
 import { Picker } from '@react-native-picker/picker'
 const Form = () => {
+  const [moneda, setMoneda] = useState('');
+  const [criptoMoneda, setCriptoMoneda] = useState('');
+
+  const obtenerMoneda = moneda =>{
+    setMoneda(moneda)
+  } 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Moneda</Text>
-      <View style={{ width: '100%', }}>
 
-        <Picker
-          
-          itemStyle={{ height: 120 }}
-        >
-          <Picker.Item label="- Seleccione -" value="" />
-          <Picker.Item label="Dolar de Estados Unidos" value="USD" />
-          <Picker.Item label="Peso Mexicano" value="MXN" />
-          <Picker.Item label="Euro" value="EUR" />
-          <Picker.Item label="Libra Esterlina" value="GBP" />
-        </Picker>
+      <Picker 
+        selectedValue={moneda}
+        itemStyle={{ height: 120 }} 
+        onValueChange={moneda=> obtenerMoneda(moneda)}
+      >
+        <Picker.Item label="- Seleccione -" value="" />
+        <Picker.Item label="Dolar de Estados Unidos" value="USD" />
+        <Picker.Item label="Peso Argentino" value="ARG" />
+        <Picker.Item label="Euro" value="EUR" />
+        <Picker.Item label="Libra Esterlina" value="GBP" />
+      </Picker>
 
-        
-
-      </View>
 
       <Text style={styles.text}>Criptomoneda</Text>
     </View>
