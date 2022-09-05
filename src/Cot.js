947608865b2fabf2ee2011bd8f9edf327ee8252c
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-05 01:02:05 -0300

    fixed

--- src/Cot.js
+++ src/Cot.js
@@ -6,19 +6,19 @@ const Cot = ({ resultado }) => {
   return (
     <View style={styles.containerCot}>
       <View style={styles.info}>
-        <Text style={styles.text}>{resultado.PRICE}</Text>
+        <Text style={styles.text}>Precio: {resultado.PRICE}</Text>
       </View>
       <View style={styles.info}>
         <Text style={styles.text}>Precio mas alto del dia: {resultado.HIGHDAT}</Text>
       </View>
       <View style={styles.info}>
-        <Text style={styles.text}>Precio mas bajo del dia:  {resultado.LOWDAY}</Text>
+        <Text style={styles.text}>Precio mas bajo del dia: {resultado.LOWDAY}</Text>
       </View>
       <View style={styles.info}>
-        <Text style={styles.text}>Variacion ultimas 24 horas:  {resultado.CHANGEPCT24HOUR}%</Text>
+        <Text style={styles.text}>Variacion ultimas 24 horas: {resultado.CHANGEPCT24HOUR}%</Text>
       </View>
       <View style={styles.info}>
-        <Text style={styles.text}>Ultima Actualizacion:  {resultado.LASTUPDATE}</Text>
+        <Text style={styles.text}>Ultima Actualizacion: {resultado.LASTUPDATE}</Text>
       </View>
     </View>
   )
